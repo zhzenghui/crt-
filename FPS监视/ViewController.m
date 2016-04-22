870af5a22e89aef1b90f6e23df3cad7a3b9a16e5
zenghui <zhzenghui@gmail.com>
2016-04-22 17:26:30 +0800

    runloop  fps 监控

---
+++ FPS监视/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  VVeboTableViewDemo
+//
+//  Created by Johnil on 15/5/25.
+//  Copyright (c) 2015年 Johnil. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "VVeboTableView.h"
+
+#import "YYFPSLabel.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController {
+    VVeboTableView *tableView;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    tableView = [[VVeboTableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    tableView.contentInset = UIEdgeInsetsMake(20, 0, 0, 0);
+    tableView.scrollIndicatorInsets = tableView.contentInset;
+    [self.view addSubview:tableView];
+    
+    UIToolbar *statusBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 20)];
+    [self.view addSubview:statusBar];
+    
+    
+    YYFPSLabel *fl = [[YYFPSLabel alloc] initWithFrame:CGRectMake(100, 99, 0, 0)];
+    [self.view addSubview:fl];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ FPS监视/YYFPSLabel.h
@@ -0,0 +1,13 @@
+//
+//  YYFPSLabel.h
+//  VVeboTableViewDemo
+//
+//  Created by xy on 16/4/22.
+//  Copyright © 2016年 Johnil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYFPSLabel : UILabel
+
+@end
